5f7e79e3223009596c9935dfb3d23d1166ababd6
8ucchiman <yukibuchi17@keio.jp>
2023-05-19 18:51:15 +0900

    shader added

--- libraries/opengl/linux/shader/main.cpp
+++ libraries/opengl/linux/shader/main.cpp
@@ -9,7 +9,7 @@
 
 
 #include <iostream>
-#include "glad/include/glad/glad.h"
+#include <glad/glad.h>
 #include "GLFW/glfw3.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
